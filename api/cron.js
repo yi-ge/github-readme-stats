c9879417a406da4906f894b4dec4123125c4075b
yi-ge <a@wyr.me>
2023-03-24 14:27:18 +0800

    feat: 添加定时任务

---
+++ api/cron.js
@@ -0,0 +1,25 @@
+const https = require('https');
+
+export default function handler (_req, res) {
+  const timestamp = Date.now();
+  const options = {
+    hostname: 'github-readme-stats-lyart-nine-64.vercel.app',
+    path: `/api/top-langs/?username=yi-ge&layout=compact&theme=dark&langs_count=10&hide=javascript,html,css,cmake,shell,tsql,tcl,scss,less,stylus,batchfile,vue&exclude_repo=weixin_shop,YYSECRET&cache=false&timestamp=${timestamp}`,
+    method: 'GET'
+  };
+
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', d => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', error => {
+    console.error(error);
+  });
+
+  req.end();
+  res.status(200).end('Hello Cron!');
+}
