5c0a31cbe82d9b645afb67eeb51e8765908efe7b
yi-ge <a@wyr.me>
2023-04-09 10:15:29 +0800

    fix: exclude repo

--- api/cron.js
+++ api/cron.js
@@ -6,7 +6,7 @@ import { fetchTopLanguages } from "../src/fetchers/top-languages-fetcher.js";
 
 export default async function handler (_req, res) {
   const username = 'yi-ge'
-  const exclude_repo = 'weixin_shop,YYSECRET'
+  const exclude_repo = 'weixin_shop,YYSECRET,JAVA-MyBatis-Test,JAVA-MyBatis-Test,GUISUVPay,fe9-home,bluewhale-web,amis-admin-webpack,todolist'
   const topLangs = await fetchTopLanguages(
     username,
     parseArray(exclude_repo),
